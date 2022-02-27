c58c9c174890f283e72b3bfbbad0676af46427c5
Paul Moreland <paul@pamosystems.com>
2022-02-27 21:38:25 +0100

    Added dynamic page icons

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        breakpoints: {
+          xlarge: 2560,
+          large: 1000,
+          medium: 750,
+          small: 500
+        },
+      },
+    },
+  });
