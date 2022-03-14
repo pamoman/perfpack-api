03e1b1232f0d9d592e5873e65d8ac4fa105c8af4
Paul Moreland <paul@pamosystems.com>
2022-03-15 00:27:53 +0100

    Added nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,31 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        breakpoints: {
-          xlarge: 2560,
-          large: 1000,
-          medium: 750,
-          small: 500
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: env('SMTP_HOST', 'smtp.gmail.com'),
+                port: env('SMTP_PORT', 465),
+                auth: {
+                    type: "OAuth2",
+                    user: env('SMTP_USERNAME'),
+                    serviceClient: env('SMTP_CLIENT_ID'),
+                    privateKey: env('SMTP_PRIVATE_KEY')
+                },
+            },
+            settings: {
+                defaultFrom: env('EMAIL_USER', 'info@performancepackuf.com'),
+                defaultReplyTo: env('EMAIL_USER', 'info@performancepackuf.com'),
+            },
         },
-      },
     },
-  });
+    upload: {
+        config: {
+            breakpoints: {
+                xlarge: 2560,
+                large: 1000,
+                medium: 750,
+                small: 500
+            },
+        },
+    },
+});
---
+++ src/api/order/content-types/order/lifecycles.js
@@ -0,0 +1,107 @@
+/*
+ * Lifecycles - Order
+ */
+
+const newOrderEmail = {
+    subject: 'PerformancePack Orderbekräftelse',
+    text: `
+        Grattis <%= order.firstname %>!
+
+        Ditt ordernummer är: <%= order.id %>
+
+        Du har beställt följande produkter:
+        
+        <%= order.basketText %>
+
+        Total: <%= order.total %>
+
+        Betalninsstatus: <%= order.status %>
+
+        Hälsningar PerformancePack
+    `,
+    html: `<h1>Grattis <%= order.firstname %>!</h1>
+        <p>Ditt ordernummer är: <%= order.id %></p>
+        <p>Du har beställt följande produkter:</p>
+        <%= order.basketHTML %>
+        <p>Total: <%= order.total %></p>
+        <p>Betalninsstatus: <%= order.status %></p>
+
+        <p>Hälsningar PerformancePack</p>
+    `,
+};
+
+const updateOrderEmail = {
+    subject: 'Ändrad Status',
+    text: `
+        Ändrad Betalninsstatus
+
+        Ditt ordernummer är: <%= order.id %>
+
+        Du har beställt följande produkter:
+        
+        <%= order.basketText %>
+
+        Total: <%= order.total %> kr
+
+        Betalninsstatus: <%= order.status %>
+
+        Hälsningar PerformancePack
+    `,
+    html: `<h1>Ändrad Betalningsstatus</h1>
+        <p>Ditt ordernummer är: <%= order.id %></p>
+        <p>Du har beställt följande produkter:</p>
+        <%= order.basketHTML %>
+        <p>Betalninsstatus: <%= order.status %></p>
+
+        <p>Hälsningar PerformancePack</p>
+    `,
+};
+
+const sendMail = async (template, result) => {
+    const { id } = result;
+
+    const order = await strapi.entityService.findOne('api::order.order', id, {
+        populate: { items: true },
+    });
+
+    const { firstname, lastname, email, total, paid, items } = order;
+
+    const status = paid ? "Betalning verifierad." : "Din betalning inväntar verifering.";
+    let basketHTML = ``;
+    let basketText = ``;
+
+    items.forEach(item => {
+        const { name, quantity, price, subtotal } = item;
+
+        basketHTML += `<p>${quantity}st ${name} @ ${price}/st = ${subtotal}kr</p>`;
+        basketText += `${quantity}st ${name} @ ${price}/st = ${subtotal}kr\n`
+    });
+
+    await strapi.plugins['email'].services.email.sendTemplatedEmail(
+        {
+          to: email,
+        },
+        template,
+        {
+          order: {
+              id,
+              status,
+              basketHTML,
+              basketText,
+              firstname,
+              lastname,
+              email,
+              total
+          },
+        }
+      );
+};
+
+module.exports = {
+    async afterCreate({ result }) {
+        await sendMail(newOrderEmail, result);
+    },
+    async afterUpdate({ result, params: { data } }) {
+        await sendMail(updateOrderEmail, result);
+    }
+};
